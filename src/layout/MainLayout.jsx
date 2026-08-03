ac9bf0c33b165d98d87e87bd426586a670d994d1
Ali Hassan <alihassanedjibrine009@gmail.com>
2026-08-03 23:08:51 +0100

    termine

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/common/Navbar";
-import Footer from "../components/common/footer";
+import Footer from "../components/common/Footer";
 import "../components/common/navbar.css";
 import "../components/common/footer.css";
 
